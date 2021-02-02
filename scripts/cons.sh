387a6a8fca7614a6e1d6c161156b90c5f1a15f77
klajbar <david@klajbar.com>
2021-02-02 12:10:41 +0100

    Change consumption calculate save script

--- scripts/cons.sh
+++ scripts/cons.sh
@@ -1,3 +1,8 @@
-datenow=`date +%d%m%y`
-[[ $2 = "unknown" ]] && value="0" || value="$2"
-echo "$value" > "log/$1/consumption_$datenow.log"
+### datenow=`date +%d%m%y`
+### [[ $2 = "unknown" ]] && value="0" || value="$2"
+### echo "$value" > "log/$1/consumption_$datenow.log"
+
+datenow=`date +%y.%m.%d`
+[[ $2 = "unknown" ]] && value=0 || value=$2
+echo "$datenow"
+curl -X PUT "localhost:5500/cons/$1" --data "{\"device\":\"$1\",\"watt\":$value,\"date\":\"$datenow\"}"
--- scripts/get_yesterday_cons.sh
+++ scripts/get_yesterday_cons.sh
@@ -1,2 +1,6 @@
-yesterday=$(TZ=UTC+24 date +%d%m%y)
-[ -r /config/log/$1/consumption_$yesterday.log ] && cat /config/log/$1/consumption_$yesterday.log
+### yesterday=$(TZ=UTC+24 date +%d%m%y)
+### [ -r /config/log/$1/consumption_$yesterday.log ] && cat /config/log/$1/consumption_$yesterday.log
+
+yesterday=$(TZ=UTC+24 date +%y.%m.%d)
+curl localhost:5500/cons/$1/$yesterday?data=watt
+
